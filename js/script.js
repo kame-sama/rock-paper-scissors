abf8f0890955344d44e1be41f0ac4e20d6ab07c3
kame-sama <ad.bodeev@gmail.com>
2023-11-22 07:02:38 +0500

    Add playRound

--- js/script.js
+++ js/script.js
@@ -3,3 +3,35 @@ function getComputerChoice() {
     const index = Math.floor(Math.random() * 3);
     return shapes[index];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    
+    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+        return 'Wrong Input!';
+    }
+
+    if (playerSelection == computerSelection) {
+        return 'A Tie! Start over';
+    } else {
+        if (playerSelection == 'rock') {
+            if (computerSelection == 'scissors') {
+                return 'You Won! Rock beats Scissors';
+            } else {
+                return 'You Lose! Paper beats Rock'
+            }
+        } else if (playerSelection == 'paper') {
+            if (computerSelection == 'rock') {
+                return 'You Won! Paper beats Rock'
+            } else {
+                return 'You Lose! Scissors beats Paper'
+            }
+        } else {
+            if (computerSelection == 'paper') {
+                return 'You Won! Scissors beats Paper';
+            } else {
+                return 'You Lose! Rock beats Scissors';
+            }
+        }
+    }
+}
